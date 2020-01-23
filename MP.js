ac4b39fafa5a428b2166e23323fd9b9b3d324d29
Jaagrav <jaagravseal03@gmail.com>
2020-01-23 18:16:26 +0530

    Update MP.js

--- MP.js
+++ MP.js
@@ -5,14 +5,18 @@ const pp = document.querySelector("#pp");
 const progressBar = document.querySelector("#progressBar");
 const song = document.querySelector("#song");
 var playing = true;
-var songs = ["Jaden Smith - Icon.mp3", "Billie Eilish - bad guy (Lyrics).mp3", "Tommee Profitt - In The End (Mellen Gi Remix).mp3", "Ross Lynch - On My Own (From Teen Beach 2).mp3"],
+var songs = ["Jaden Smith - Icon.mp3", "Billie Eilish - bad guy (Lyrics).mp3", "Tommee Profitt - In The End (Mellen Gi Remix).mp3", "Ross Lynch - On My Own (From Teen Beach 2).mp3","y2mate.com - lord_huron_the_night_we_met_lyric_video_wGF7PswOENQ.mp3","y2mate.com - idina_menzel_evan_rachel_wood_show_yourself_from_frozen_2lyric_video_md7dK5-qvHc.mp3","y2mate.com - jar_of_hearts_christina_perri_lyrics_rOYnrBuIMgA.mp3","y2mate.com - christina_perri_the_lonely_lyrics_a9YQPWqTnx4.mp3"],
   songIndex = 0;
-(thumbnails = ["https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQSCaJfLX-LEPmCHcz5fvkcZUrO2_5Z4LeaGe-HiuIYh829Q6YQ", "https://upload.wikimedia.org/wikipedia/en/3/38/When_We_All_Fall_Asleep%2C_Where_Do_We_Go%3F.png", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo1RQ_vSELHhtffeMwTehFeUa9d7N_lwHeoZeovIN77uF9p67e", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTob-tfpJQI1_J2pTsGaRh0tQT3oOUBsSdCrGbh790J1Was-koe"]),
+(thumbnails = ["https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQSCaJfLX-LEPmCHcz5fvkcZUrO2_5Z4LeaGe-HiuIYh829Q6YQ", "https://upload.wikimedia.org/wikipedia/en/3/38/When_We_All_Fall_Asleep%2C_Where_Do_We_Go%3F.png", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo1RQ_vSELHhtffeMwTehFeUa9d7N_lwHeoZeovIN77uF9p67e", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTob-tfpJQI1_J2pTsGaRh0tQT3oOUBsSdCrGbh790J1Was-koe","https://m.media-amazon.com/images/I/915Rma--XpL._SS500_.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQykc1WORFyzaAuvEcqWc6EsCaroo-i_QcmZFML0o4gteurxuQP","https://upload.wikimedia.org/wikipedia/en/f/f9/Jarofhearts.jpg","https://m.media-amazon.com/images/I/71a0frsfbTL._SS500_.jpg"]),
   (songNames = [
     "Jaden Smith - Icon",
-    "Billie Eilish - Bad Guy",
-    "Linkin Park - In The End",
-    "Ross Lynch - On My Own"
+    "Bad Guy - Billie Eilish",
+    "In The End - Linkin Park",
+    "On My Own - Ross Lynch",
+    "Night We Met - Lord Huron",
+    "Show Yourself - Idina Menzel",
+    "Jar Of Hearts \nChristina Perri"
+    "The Lonely \nChristina Perri"
   ]);
 
 function playPause() {
@@ -34,7 +38,7 @@ function playPause() {
 
 function nextSong() {
   songIndex++;
-  if (songIndex > 3) songIndex = 0;
+  if (songIndex > songs.length-1) songIndex = 0;
   document.querySelector("#song").src = songs[songIndex];
   document.querySelector("#thumbnail").src = thumbnails[songIndex];
   document.querySelector("#BG").src = thumbnails[songIndex];
@@ -44,7 +48,7 @@ function nextSong() {
 
 function previousSong() {
   songIndex--;
-  if (songIndex < 0) songIndex = 3;
+  if (songIndex < 0) songIndex = songs.length-1;
   document.querySelector("#song").src = songs[songIndex];
   document.querySelector("#thumbnail").src = thumbnails[songIndex];
   document.querySelector("#BG").src = thumbnails[songIndex];
